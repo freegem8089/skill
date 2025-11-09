3f0a6de7809f473a2c80f63e251f9b3211262be4
freegem8089 <freegem8089@gmail.com>
2025-11-09 23:14:57 +0530

    Add files via upload

---
+++ zombie.c
@@ -0,0 +1,20 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+pid_t pid;
+if((pid=fork()) < 0)
+printf("\tfork error\n");
+else
+if(pid==0)
+printf("child process id is %d\n",getpid());
+else
+{
+sleep(10);
+printf("*****parent\n");
+system("ps -axj | tail");
+}
+exit(0);
+}
